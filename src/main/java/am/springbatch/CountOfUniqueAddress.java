354cb2484a48aea5bde75585c5de07bda7d49fe1
LevonSirunyan <levonsirunyan@gmail.com>
2021-11-11 22:26:51 +0400

    Create project , first commit

---
+++ src/main/java/am/springbatch/CountOfUniqueAddress.java
@@ -0,0 +1,18 @@
+package am.springbatch;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountOfUniqueAddress implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CountOfUniqueAddress.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Hello!!!");
+    }
+}
